79612e0ae7f0fb06556d3666b02c1c514227c057
T.J. Hawkins <t.j.hawkins@icloud.com>
2022-04-13 19:18:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,11 @@
 module "network" {
   source  = "app.terraform.io/hawktail/network/google"
   version = "3.4.0"
+
+  project_id   = var.project
+  network_name = "example-vpc"
+  routing_mode = "GLOBAL"
+
   subnets = [
     {
       subnet_name   = "gaurav-subnet"
